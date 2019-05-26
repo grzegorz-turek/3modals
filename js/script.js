fc47bc9d2120574bc99524f921471c3d4837511b
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-05-26 11:13:39 +0200

    Test 3

--- js/script.js
+++ js/script.js
@@ -1,23 +1,35 @@
 'use strict';
-(function(){ 
-	/* W kodzie HTML i CSS dodaliśmy style dla prostego modala, który będzie zawsze wyśrodkowany w oknie. 
-	
-	Teraz wystarczy napisać funkcję otwierającą modal:
-	*/
+(function(){
 	
 	var showModal = function(event){
 		event.preventDefault();
 		document.querySelector('#modal-overlay').classList.add('show');
+		document.querySelector(x).classList.add('show'); // 2. x -> y
+		document.getElementById("result").innerHTML += ' var x: ' + x + ', var i: ' + i + '<br>'; // 2. x -> y
 	};
 	
-	// Mimo, że obecnie mamy tylko jeden link, stosujemy kod dla wielu linków. W ten sposób nie będzie trzeba go zmieniać, kiedy zechcemy mieć więcej linków lub guzików otwierających modale
-	
 	var modalLinks = document.querySelectorAll('.show-modal');
-	
-	for(var i = 0; i < modalLinks.length; i++){
+	for (var i = 0; i < modalLinks.length; i++) {
 		modalLinks[i].addEventListener('click', showModal);
-	}
-	
+		var x = modalLinks[i].getAttribute("href"); // 1.
+		//var x = document.getElementsByTagName("a") // 2.
+		//var y = x.getAttribute("href"); // 2.
+	};
+
+	//for (var i = 0; i < modalLinks.length; i++) { // 3.
+	//	var y = modalLinks[i].getAttribute("href"); // 3.
+	//} // 3.
+
+// addEventListener poza pętlą
+// 1/ funkcja w drugirj pętli
+// 2. przenieść getAttrib do showModal
+// 3. obiekt/tablica
+// 4. if w pierwszej / drugiej pętli?
+
+
+	//var x = document.getElementsByTagName("a")[5].getAttribute("href");  // DZIAŁA
+
+
 	// Dodajemy też funkcję zamykającą modal, oraz przywiązujemy ją do kliknięć na elemencie z klasą "close". 
 
 	var hideModal = function(event){
@@ -44,11 +56,4 @@
 			event.stopPropagation();
 		});
 	}
-	
-	/* I to wszystko - mamy już działający modal! 
-	
-	ĆWICZENIE: 
-	Zmień funkcję showModal tak, aby w momencie wyświetlania była zmieniana treść nagłówka na dowolną inną, np. "Modal header". 
-	*/
-	
 })(); 
