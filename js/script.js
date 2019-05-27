98d020859f5b1d7a8e23572cc52b617cf55dfa9c
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-05-28 00:33:21 +0200

    Final

--- js/script.js
+++ js/script.js
@@ -1,47 +1,42 @@
 'use strict';
 (function(){
 	
-	var showModal = function(event){
+	var getSelector = function (index) {
+		switch(index) {
+			case 0:
+			case 1:
+				return '#modal-one';
+			case 2:
+			case 3:
+				return '#modal-two';
+			case 4:
+			case 5:
+				return '#modal-three';
+			default:
+				 return '#modal-one';
+		}
+	}
+
+	var showModal = function(event, i){
 		event.preventDefault();
 		document.querySelector('#modal-overlay').classList.add('show');
-		document.querySelector(x).classList.add('show'); // 2. x -> y
-		document.getElementById("result").innerHTML += '<br> var x: ' + x + ', var index: ' + index; // 2. x -> y
-
+		var selector = getSelector(i)
+		document.querySelector(selector).classList.add('show'); // 2. x -> y
 	};
 	
 	var modalLinks = document.querySelectorAll('.show-modal');
 
-	var logModalLinksIndex = function(modalLinksIndex) {
-		console.log('modalLinksIndex:', modalLinksIndex);
-		document.getElementById('result2').innerHTML += '<br> modalLinksIndex: '+ modalLinksIndex;
-
-
-
+//	var logModalLinksIndex = function(modalLinksIndex) {
+//		console.log('modalLinksIndex:', modalLinksIndex);
+//		document.getElementById('result2').innerHTML += '<br> modalLinksIndex: '+ modalLinksIndex;
+//	}
 
 	modalLinks.forEach(function(arg1, index) {
 		arg1.addEventListener('click', function() {
-			  logModalLinksIndex(index);
-			  showModal(event);
+//			  logModalLinksIndex(index);
+			  showModal(event, index);
 		});
 	});
-
-/*
-	for (var i = 0; i < modalLinks.length; i++) {
-		modalLinks[i].addEventListener('click', showModal);
-		var x = modalLinks[i].getAttribute("href"); // 1.
-	};
-*/
-
-// addEventListener poza pętlą
-// 1/ funkcja w drugirj pętli
-// 2. przenieść getAttrib do showModal
-// 3. obiekt/tablica
-// 4. if w pierwszej / drugiej pętli?
-
-//var x = document.getElementsByTagName("a")[5].getAttribute("href");  // DZIAŁA
-
-
-	// Dodajemy też funkcję zamykającą modal, oraz przywiązujemy ją do kliknięć na elemencie z klasą "close". 
 
 	var hideModal = function(event){
 		event.preventDefault();
@@ -49,22 +44,15 @@
 	};
 	
 	var closeButtons = document.querySelectorAll('.modal .close');
-	
 	for(var i = 0; i < closeButtons.length; i++){
 		closeButtons[i].addEventListener('click', hideModal);
 	}
 	
-	// Dobrą praktyką jest również umożliwianie zamykania modala poprzez kliknięcie w overlay. 
-	
 	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
-	
-	// Musimy jednak pamiętać, aby zablokować propagację kliknięć z samego modala - inaczej każde kliknięcie wewnątrz modala również zamykałoby go. 
-	
 	var modals = document.querySelectorAll('.modal');
-	
 	for(var i = 0; i < modals.length; i++){
 		modals[i].addEventListener('click', function(event){
 			event.stopPropagation();
 		});
 	}
-})(); 
+})();
